380da33cf9ffa5959628812b85603e1bafc597ce
giulio <giulio09@marcov.it>
2021-02-12 12:32:56 +0000

    tesnroboard + optuna

--- search_parameters.py
+++ search_parameters.py
@@ -4,25 +4,28 @@ import matplotlib.pyplot as plt
 from IPython import display as ipythondisplay
 
 import optuna
+from optuna.integration.tensorboard import TensorBoardCallback
 
 import gym
 import gym_duckietown
 
-from wrappers import ObsWrapper
-
 from stable_baselines.common.vec_env import VecFrameStack
-from stable_baselines import A2C
 from stable_baselines import DDPG
+from stable_baselines import A2C
 from stable_baselines.common.policies import CnnLstmPolicy
 from stable_baselines.common.evaluation import evaluate_policy
 
 from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
- 
+
+from wrappers import ObsWrapper
+
 map_name = "Duckietown-small_loop-v0" #@param ['Duckietown-straight_road-v0','Duckietown-4way-v0','Duckietown-udem1-v0','Duckietown-small_loop-v0','Duckietown-small_loop_cw-v0','Duckietown-zigzag_dists-v0','Duckietown-loop_obstacles-v0','Duckietown-loop_pedestrians-v0']
 display = Display(visible=0, size=(1400, 900))
 display.start()
 env = gym.make(map_name, accept_start_angle_deg=4)
 env = ObsWrapper(env)
+tensorboard_callback = TensorBoardCallback("./optuna/", metric_name="optuna_mean_reward")
+
 
 def model_DDPG(gamma, tensorboard="./optuna/"):
   n_actions = env.action_space.shape[-1]
@@ -52,12 +55,12 @@ def model_A2C(gamma, tensorboard="./optuna/"):
       tensorboard_log=tensorboard
     )
  
-def objective(trial):
+def objective(trial: optuna.trial.Trial)  -> float:
 
   gamma = trial.suggest_float("learning_rate", 0.4, 0.99)
   
   model = model_DDPG(gamma)
-  model.learn(total_timesteps=int(5e4))
+  model.learn(total_timesteps=int(1e4))
   mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=100)
   print(gamma)
   print(mean_reward)
@@ -67,4 +70,4 @@ def objective(trial):
 
 if __name__ == "__main__":
   study = optuna.create_study(direction='maximize')
-  study.optimize(objective, n_trials=20)
+  study.optimize(objective, n_trials=20, callbacks=[tensorboard_callback])
