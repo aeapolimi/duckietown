c69036d068ed1fdbecbb7a9d70aedef831428842
giulio <giulio09@marcov.it>
2021-02-24 18:05:09 +0000

    Fix load a2c

--- viewer.py
+++ viewer.py
@@ -3,15 +3,21 @@ from stable_baselines.common.vec_env import VecVideoRecorder, DummyVecEnv
 from utils.wrappers import ObsWrapper, CropResizeWrapper, MyRewardWrapper
 from utils.duckie_wrappers import NormalizeWrapper, ImgWrapper, DtRewardWrapper, ActionWrapper, ResizeWrapper
 
+from stable_baselines import A2C
+from stable_baselines import DDPG
+
 import gym
 import gym_duckietown
 
 map_name = "Duckietown-small_loop-v0"
-
 model_to_be_loaded = "a2cDuckietown-small_loop-v060000.0"
+modello = "a2c"
 
 if __name__=="__main__":
-    model = A2C.load("../models/"+model_to_be_loaded)
+    if modello == "a2c":
+        model = A2C.load("../models/"+model_to_be_loaded)
+    elif modello == "ddpg":
+        model = DDPG.load("../models/"+model_to_be_loaded)
     env = gym.make(map_name)
     env = CropResizeWrapper(env)
     env = ObsWrapper(env)
